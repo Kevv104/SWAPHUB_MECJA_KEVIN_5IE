b9d8138ddae711f028830bf8ed57d23a60c6d85c
Kevin Mecja <mecja.kevin.studente@itispaleocapa.it>
2026-04-23 15:57:50 +0000

    Modifica tenants_config, con 20 regioni italiane

--- LOGIN_DATABASE/config/tenants_config.php
+++ LOGIN_DATABASE/config/tenants_config.php
@@ -1,21 +1,25 @@
 <?php
  return [
     'tenants' => [
-        1 => [
-            'id'   => 1,
-            'name' => 'bergamo',
-            'city' => 'Bergamo',
-        ],
-        2 => [
-            'id'   => 2,
-            'name' => 'milano',
-            'city' => 'Milano',
-        ],
-        3 => [
-            'id'   => 3,
-            'name' => 'torino',
-            'city' => 'Torino',
-        ],
+        1 => ['id' => 1, 'name' => 'piemonte', 'city' => 'Piemonte'],
+        2 => ['id' => 2, 'name' => 'valle_aosta', 'city' => 'Valle d\'Aosta'],
+        3 => ['id' => 3, 'name' => 'lombardia', 'city' => 'Lombardia'],
+        4 => ['id' => 4, 'name' => 'liguria', 'city' => 'Liguria'],
+        5 => ['id' => 5, 'name' => 'friuli_venezia_giulia', 'city' => 'Friuli Venezia Giulia'],
+        6 => ['id' => 6, 'name' => 'veneto', 'city' => 'Veneto'],
+        7 => ['id' => 7, 'name' => 'emilia_romagna', 'city' => 'Emilia-Romagna'],
+        8 => ['id' => 8, 'name' => 'toscana', 'city' => 'Toscana'],
+        9 => ['id' => 9, 'name' => 'umbria', 'city' => 'Umbria'],
+        10 => ['id' => 10, 'name' => 'marche', 'city' => 'Marche'],
+        11 => ['id' => 11, 'name' => 'lazio', 'city' => 'Lazio'],
+        12 => ['id' => 12, 'name' => 'abruzzo', 'city' => 'Abruzzo'],
+        13 => ['id' => 13, 'name' => 'molise', 'city' => 'Molise'],
+        14 => ['id' => 14, 'name' => 'campania', 'city' => 'Campania'],
+        15 => ['id' => 15, 'name' => 'puglia', 'city' => 'Puglia'],
+        16 => ['id' => 16, 'name' => 'basilicata', 'city' => 'Basilicata'],
+        17 => ['id' => 17, 'name' => 'calabria', 'city' => 'Calabria'],
+        18 => ['id' => 18, 'name' => 'sicilia', 'city' => 'Sicilia'],
+        19 => ['id' => 19, 'name' => 'sardegna', 'city' => 'Sardegna'],
     ],
 ];
 
